2ae8c1536c831cbee2695392abe8cf71459fec19
ckarpovich <c.karpovich@gwmail.gwu.edu>
2026-09-09 18:13:42 -0400

    Create Chris_Karpovich_HW1.py
    HW 1

---
+++ Chris_Karpovich_HW1.py
@@ -0,0 +1,15 @@
+# Chris Karpovich - Week 1: your first Streamlit app
+# Run with:  streamlit run Chris_Karpovich_HW1.py
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("mlbstats.csv")
+df = df[["Tm", "HR"]]
+df = df.sort_values("HR")
+
+st.title("MLB Home Runs by Team") 
+st.caption("Home Run Distribution by MLB Team in 2026 Season (So Far)")
+
+st.dataframe(df)
+st.bar_chart(df, x="Tm", y="HR")
